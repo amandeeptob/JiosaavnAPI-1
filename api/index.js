b37564187cb3f47821cb27641d999ff0d86c3f1d
amandeeptob <105418098+amandeeptob@users.noreply.github.com>
2022-05-16 20:16:45 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const package = require("../package.json");
 module.exports = async (req, res) => {
   res.json({
     alive: true,
-    made_by: "https://github.com/cachecleanerjeet",
     version: package.version,
     docs: `${process.env.APP_URL || "http://localhost:3000"}/docs`,
   });
